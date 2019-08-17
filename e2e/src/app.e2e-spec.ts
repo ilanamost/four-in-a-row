771dbd665a317eea42f9a9ec169673c3abf3e55f
Ilana Mostovski <ilana.mostovski@gmail.com>
2019-08-17 17:24:46 +0300

    change application name

--- e2e/src/app.e2e-spec.ts
+++ e2e/src/app.e2e-spec.ts
@@ -10,7 +10,7 @@ describe('workspace-project App', () => {
 
   it('should display welcome message', () => {
     page.navigateTo();
-    expect(page.getTitleText()).toEqual('Welcome to x-mix-drix!');
+    expect(page.getTitleText()).toEqual('Welcome to four-in-a-row!');
   });
 
   afterEach(async () => {
--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'x-mix-drix'`, () => {
+  it(`should have as title 'four-in-a-row'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('x-mix-drix');
+    expect(app.title).toEqual('four-in-a-row');
   });
 
   it('should render title in a h1 tag', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('h1').textContent).toContain('Welcome to x-mix-drix!');
+    expect(compiled.querySelector('h1').textContent).toContain('Welcome to four-in-a-row!');
   });
 });
